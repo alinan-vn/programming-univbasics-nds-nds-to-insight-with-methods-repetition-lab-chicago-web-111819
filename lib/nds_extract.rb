e308b037d906d5efc6a2e0bf94421501fb68a852
Alejandro Linan <jandrolinan@gmail.com>
2019-11-12 17:46:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,31 +35,31 @@ def list_of_directors(source)
   # Write this implementation
 end
 
-# def total_gross(source)
-#   # Write this implementation
-#   #
-#   # Should use methods:
-#   # 1. directors_totals: returns a Hash of { dir_name => gross }
-#   # 2. list_of_directors: names provides an Array of directors names (use
-#   #
-#   # Visit each key (i.e. director name), look up the value in the hash
-#   # returned by directors_totals, and add it to a running total. When done,
-#   # return the total
+def total_gross(source)
+  # Write this implementation
+  #
+  # Should use methods:
+  # 1. directors_totals: returns a Hash of { dir_name => gross }
+  # 2. list_of_directors: names provides an Array of directors names (use
+  #
+  # Visit each key (i.e. director name), look up the value in the hash
+  # returned by directors_totals, and add it to a running total. When done,
+  # return the total
   
-#   total = 0 
-#   hash = directors_totals(source)
-#   names = list_of_directors(source)
-#   first_index = 0 
-#   while first_index < list_of_directors.length do 
-#     name_order = names[first_index]
-#     number = hash[name_order]
-#     total += number
+  total = 0 
+  hash = directors_totals(source)
+  names = list_of_directors(source)
+  first_index = 0 
+  while first_index < list_of_directors.length do 
+    name_order = names[first_index]
+    number = hash[name_order]
+    total += number
     
-#     first_index += 1 
+    first_index += 1 
   
-#   end 
+  end 
   
-#   return total
-# end
+  return total
+end
 
 
